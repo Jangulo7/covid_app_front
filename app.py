0f67ea9b76d6ebc917ceecaf5e7e82c67f889f6c
Johanna <johanna.angulo@gmail.com>
2025-08-14 18:02:50 +0000

    UI change

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ with col1:
 with col2:
     st.markdown(
         f"""
-        **Developed by:** [Johanna](https://www.linkedin.com/in/yourprofile)  
+        **Developed by:** [Johanna](https://www.linkedin.com/in/johannaangulo)  
         📅 March, {datetime.now().year}  
         **Contact:** [![LinkedIn](https://img.icons8.com/color/48/000000/linkedin.png)](https://www.linkedin.com/in/yourprofile)
         """,
